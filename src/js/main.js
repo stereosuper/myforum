de768a02b134a19c20bc456466dd1e865c621e80
Elisabeth Hamel <elisabeth@stereosuper.fr>
2019-04-24 18:19:29 +0200

    :art: form navigation

--- src/js/main.js
+++ src/js/main.js
@@ -23,6 +23,32 @@ const loadHandler = () => {
     $('#burger').on('click', function(){
         $('body').toggleClass('nav-header-open');
     });
+
+    const form = $('#form');
+    const steps = form.find('.form-step');
+    const nbSteps = steps.length;
+    const formNav = $('#form-nav');
+
+    const moveForm = (nbStep, dir) => {
+        if( steps.eq(nbStep).hasClass('active') ) return;
+
+        if( dir === 'next' ){
+            TweenLite.to(form, 0.3, {x: '-=100%'});
+            form.find('.active').removeClass('active').next().addClass('active');
+        }else if( dir === 'prev'){
+            TweenLite.to(form, 0.3, {x: '+=100%'});
+            form.find('.active').removeClass('active').prev().addClass('active');
+        }
+        
+        formNav.find('button').removeClass('active').eq(form.find('.active').index()).addClass('active');
+    };
+
+    $('#form-next').on('click', function(){
+        moveForm(nbSteps - 1, 'next');
+    });
+    $('#form-prev').on('click', function(){
+        moveForm(0, 'prev');
+    });
 }
 
 if (document.readyState === 'complete') {
