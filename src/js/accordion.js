f142aa2c72ef501892aa499d8a614b3bddfb56e4
Alphability <albanmezino@gmail.com>
2019-07-18 13:41:57 +0200

    🎨 @stereorepo/sac: SuperError class
    Signed-off-by: Alphability <albanmezino@gmail.com>

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -3,9 +3,9 @@ import { Accordion } from '@stereorepo/accordion';
 const accordionHandler = () => {
     const accordions = new Accordion({
         containerSelector: '.js-accordion',
-        clickedSelector: 'h3',
-        contentWrapperSelector: '.content-wrapper',
-        contentSelector: '.content'
+        clickedSelector: '.js-accordion-title',
+        contentWrapperSelector: '.js-content-wrapper',
+        contentSelector: '.js-content'
     });
 
     accordions.initializeAccordions();
