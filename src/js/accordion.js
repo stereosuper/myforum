39f6dd05c442ea5b0100fcd13bca0d35a0998b48
Alphability <albanmezino@gmail.com>
2019-07-19 14:08:29 +0200

    🎨 nested accordions
    Signed-off-by: Alphability <albanmezino@gmail.com>

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,4 +1,17 @@
+import { query } from '@stereorepo/sac';
 import { Accordion } from '@stereorepo/accordion';
+
+// HACK: Simulating click
+const simulateClick = function(elem) {
+    // Create our event (with options)
+    const evt = new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+        view: window
+    });
+    // If cancelled, don't dispatch our event
+    const canceled = !elem.dispatchEvent(evt);
+};
 
 const accordionHandler = () => {
     const accordions = new Accordion({
@@ -11,6 +24,10 @@ const accordionHandler = () => {
     });
 
     accordions.initializeAccordions();
+
+    // HACK: Testing accordion insides
+    // const [click] = query({ selector: '.js-accordion-title' });
+    // simulateClick(click);
 };
 
 export default accordionHandler;
